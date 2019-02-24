328ac9b14cb0a18e4a961e271d174b1c1e1d71e7
Richard Yang <richard@yangrichard.com>
2019-02-24 01:16:58 -0700

    Fix initial countdown update delay; smooth navbar animation

--- js/ui.js
+++ js/ui.js
@@ -159,25 +159,38 @@
 })(jQuery);
 
 
-// Countdown timer
-var countDownDate = new Date("May 25, 2019 10:00:00").getTime();
-var x = setInterval(function() {
-	var now = new Date().getTime();
-	// Find the distance between now and the countdown date
-	var distance = countDownDate - now;
-	// Time calculations and prefix "0" if needed
-	var days = Math.floor(distance / (1000*60*60*24));
-	var hours = ("0" + Math.floor((distance % (1000*60*60*24)) / (1000*60*60))).slice(-2);
-	var minutes = ("0" + Math.floor((distance % (1000*60*60)) / (1000*60))).slice(-2);
-	var seconds = ("0" + Math.floor((distance % (1000*60)) / 1000)).slice(-2);
-	// Display
-	document.getElementById("countdown").innerHTML = days + " days " + hours + ":" + minutes + ":" + seconds;
-	// If the count down is finished, write some text
-	if (distance < 0) {
-		clearInterval(x);
-		document.getElementById("countdown").innerHTML = "0 days 00:00:00";
-	}
-}, 1000);
+// For homepage
+if (window.location.pathname == "/") {
+    // Countdown timer
+    var countDownDate = new Date("May 25, 2019 10:00:00").getTime();
+    (function updateCountdown() {
+        var now = new Date().getTime();
+    	// Find the distance between now and the countdown date
+    	var distance = countDownDate - now;
+    	// Time calculations and prefix "0" if needed
+    	var days = Math.floor(distance / (1000*60*60*24));
+    	var hours = ("0" + Math.floor((distance % (1000*60*60*24)) / (1000*60*60))).slice(-2);
+    	var minutes = ("0" + Math.floor((distance % (1000*60*60)) / (1000*60))).slice(-2);
+    	var seconds = ("0" + Math.floor((distance % (1000*60)) / 1000)).slice(-2);
+    	// Display
+    	document.getElementById("countdown").innerHTML = days + " days " + hours + ":" + minutes + ":" + seconds;
+    	// If the count down is finished, write some text
+    	if (distance < 0) {
+    		clearInterval(x);
+    		document.getElementById("countdown").innerHTML = "0 days 00:00:00";
+    	}
+        setTimeout(updateCountdown, 1000);
+    })();
+
+    // Shrink navbar on scroll
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 50) {
+            $('nav').addClass('navbar-shrink');
+        } else {
+            $('nav').removeClass('navbar-shrink');
+        }
+    });
+} else if (window.location.pathname != "/") $('nav').addClass('navbar-shrink');
 
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
