c8f835051e4efc7f854166a235ef0ffcaef38966
Richard Yang <richard@yangrichard.com>
2019-05-25 13:14:15 -0600

    Disable countdown and tickets button in header

--- js/ui.js
+++ js/ui.js
@@ -161,29 +161,28 @@
 
 // For homepage
 if (window.location.pathname == "/") {
-  // Countdown timer
-  var countDownDate = new Date("May 25, 2019 10:00:00").getTime();
-  (function updateCountdown() {
-    var now = new Date().getTime();
-    // Find the distance between now and the countdown date
-    var distance = countDownDate - now;
-    // Time calculations
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    // Display
-    document.getElementById("countdownD").innerHTML = days;
-    document.getElementById("countdownH").innerHTML = hours;
-    document.getElementById("countdownM").innerHTML = minutes;
-    // If the count down is finished, write some text
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("countdownD").innerHTML = 0;
-      document.getElementById("countdownH").innerHTML = 0;
-      document.getElementById("countdownM").innerHTML = 0;
-    }
-    setTimeout(updateCountdown, 60000);
-  })();
+  // // Countdown timer
+  // var countDownDate = new Date("May 25, 2019 10:00:00").getTime();
+  // (function updateCountdown() {
+  //   var now = new Date().getTime();
+  //   // Find the distance between now and the countdown date
+  //   var distance = countDownDate - now;
+  //   // Time calculations
+  //   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  //   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //   // Display
+  //   document.getElementById("countdownD").innerHTML = days;
+  //   document.getElementById("countdownH").innerHTML = hours;
+  //   document.getElementById("countdownM").innerHTML = minutes;
+  //   // If the count down is finished, write some text
+  //   if (distance < 0) {
+  //     document.getElementById("countdownD").innerHTML = 0;
+  //     document.getElementById("countdownH").innerHTML = 0;
+  //     document.getElementById("countdownM").innerHTML = 0;
+  //   }
+  //   setTimeout(updateCountdown, 60000);
+  // })();
 
   // Shrink navbar on scroll
   $(window).scroll(function () {
