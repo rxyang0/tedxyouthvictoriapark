1a61a30f98a72ca107d0a6e0175be4eb3f3dfdee
Richard Yang <richard@yangrichard.com>
2019-11-15 00:36:40 -0800

    Add speaker application page; close #19

--- js/ui.js
+++ js/ui.js
@@ -205,12 +205,14 @@ $(function () {
 // For nominate speaker form
 $('input[name="entry.67914793"]:radio').on("change", function () {
   if ($('#q1a').is(':checked')) {
+    $('#group-q1').addClass('mb-5');
     $('#group-q1-1').hide();
     $('#q1-1').prop('required', false);
     $('#group-q1-2').hide();
     $('#q1-2').prop('required', false);
     $('#group-q1-3').hide();
   } else {
+    $('#group-q1').removeClass('mb-5');
     $('#group-q1-1').show();
     $('#q1-1').prop('required', true);
     $('#group-q1-2').show();
